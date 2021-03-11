6151737c5c9b2f68e1184defc551d43a21639db3
Kim Seung Gu <agapeuni@naver.com>
2021-03-12 02:10:05 +0900

    젬프 투 플라스크 실습

--- helloworld/helloworld/__init__.py
+++ helloworld/helloworld/__init__.py
@@ -59,3 +59,11 @@ def name2():
 @app.route("/board", defaults={ "article_id": 10 })
 def board_idx(article_id):
     return "{}번 게시물을 보고 계십니다.".format(article_id)
+
+@app.route("/board", redirect_to="/new_board")
+def board():
+    return "/board URL을 호출하셨는데 실행이 안될겁니다"
+    
+@app.route("/new_board")
+def new_board():
+    return "/new_board URL이 호출되었습니다."
